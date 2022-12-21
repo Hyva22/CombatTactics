ed90ccd3d6da08cf631ea69b1d57259fe5db19b3
Tom Entelmann <tom.entelmann@gmail.com>
2022-12-21 13:08:29 +0900

    Added comments to Network project

--- Network/Network/Network/Game/PersistantObject.cs
+++ Network/Network/Network/Game/PersistantObject.cs
@@ -2,17 +2,28 @@
 
 namespace Network.Game
 {
+    /// <summary>
+    /// Attributes that should be serialized need to be public.
+    /// </summary>
     public abstract class PersistantObject
     {
         public long id;
 
-        //public long ID { get => id; set => id = value; }
-
+        /// <summary>
+        /// Converts this object to a Json string, using Newtonsoft.Json
+        /// </summary>
+        /// <returns>Json string</returns>
         public string ToJson()
         {
             return JsonConvert.SerializeObject(this);
         }
 
+        /// <summary>
+        /// Creates an object from a Json string, using Newtonsoft.Json
+        /// </summary>
+        /// <typeparam name="T">Type of the object</typeparam>
+        /// <param name="jsonString">String to convert</param>
+        /// <returns></returns>
         public static T FromJson<T>(string jsonString) where T : PersistantObject
         {
             return JsonConvert.DeserializeObject<T>(jsonString);
--- Network/Network/Network/Game/PlayerAccount.cs
+++ Network/Network/Network/Game/PlayerAccount.cs
@@ -12,14 +12,5 @@ namespace Network.Game
         public DateTime birthDay;
         public DateTime creationDate;
         public DateTime lastLogin;
-
-        //public string Email { get => email; set => email = value; }
-        //public string AccountName { get => accountName; set => accountName = value; }
-        //public string Password { get => password; set => password = value; }
-        //public string FirstName { get => firstName; set => firstName = value; }
-        //public string SurName { get => surName; set => surName = value; }
-        //public DateTime BirthDay { get => birthDay; set => birthDay = value; }
-        //public DateTime CreationDate { get => creationDate; set => creationDate = value; }
-        //public DateTime LastLogin { get => lastLogin; set => lastLogin = value; }
     }
 }
